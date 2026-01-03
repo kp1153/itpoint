87e76f06ac0d7b691739524ac499727f71840ca3
kamta prasad <kamta_tiwari@yahoo.com>
2026-01-03 18:00:22 +0530

    force deploy

---
+++ app/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
+
+export default function Page() {
+  return (
+    <main>
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+    </main>
+  );
+}
